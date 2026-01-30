b5bab5a90146a43c3f61b789811b0f6060ccd6f0
Udaya Pratap Singh Birdi <ubirdi1997@gmail.com>
2026-01-30 19:02:00 +0000

    auto-commit for 644a4c27-8a7d-4375-a0cb-637d1ff5b5b1

--- backend_test.py
+++ backend_test.py
@@ -6,7 +6,7 @@ import json
 from datetime import datetime
 
 class TiloLiveAPITester:
-    def __init__(self, base_url="https://tilo-rebrand.preview.emergentagent.com"):
+    def __init__(self, base_url="https://tilo-live-sandbox.preview.emergentagent.com"):
         self.base_url = base_url
         self.api_url = f"{base_url}/api"
         self.token = None
